b1bfd06db247b2005c46adb0ad954ae865569f0f
Filip Luac <oliveripepa1207>
2021-03-08 19:17:33 +0100

    feat: playground

--- playground/index.js
+++ playground/index.js
@@ -1,21 +1,26 @@
-const { Octokit } = require("@octokit/rest");
+const { Octokit } = require('@octokit/rest');
 
 async function search(phrase) {
   const octokit = new Octokit({
     auth: '',
   });
 
-  await octokit.search.code({
-    q: encodeURIComponent(`${phrase} in:file repo:Jaspero/bluffs`)
-  })
+  const branch = 'Jaspero/bluffs/tree/playground';
+  const path = '/playground/sample';
+
+  const {data} = await octokit.search.code({
+    q: `${encodeURIComponent(phrase) || ''}+in:file+org:Jaspero+repo:${branch}+path:${path}`
+  });
+
+  return data;
 }
 
-search()
+search(process.argv[2])
   .then(d => {
     console.log(d);
     process.exit(0)
   })
   .catch(e => {
-    console.error(e);
+    console.error(e.errors);
     process.exit(1);
   })
