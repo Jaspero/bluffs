9b9e282db0258ec0734c8ad7fc4c425cde517f81
kristiantarnaj <kristiantarnaj@gmail.com>
2021-03-11 17:40:29 +0100

    feat(): added buffs

---
+++ shop/src/components/Buff.svelte
@@ -0,0 +1,66 @@
+<script>
+    let buffs = [
+        { packName: 'Gains Buff', rarity: 'common', tier: 'Common', cost: 0.99, listings: 1000 },
+        { packName: 'Love Buff', rarity: 'legendary', tier: 'Legendary', cost: 99.99, listings: 1 },
+    ];
+    let packName = "Generic Name"
+    let rarity = "epic"
+    let tier = "Epic"
+    let cost = "4.99$"
+    let listings = 100
+</script>
+
+<style>
+    .rarity {
+        width: 100%;
+        height: 0;
+        padding-top: 100%;
+    }
+    .common {
+        border: 1px solid #646466;
+        background-color: #EBECEF;
+    }
+    .uncommon {
+        border: 1px solid #3DA1CE;
+        background-color: #E7F2FA;
+    }
+    .rare {
+        border: 1px solid #BC9C31;
+        background-color: #F4F2EA;
+    }
+    .epic {
+        border: 1px solid #8F37C6;
+        background-color: #F0E7F9;
+    }
+    .legendary {
+        border: 1px solid #d60e0e;
+        background-color: #F7E3E6;
+    }
+
+    .Common {
+        color: #646466;
+    }
+    .Uncommon {
+        color: #3DA1CE;
+    }
+    .Rare {
+        color: #BC9C31;
+    }
+    .Epic {
+        color: #8F37C6;
+    }
+    .Legendary {
+        color: #d60e0e;
+    }
+</style>
+
+{#each buffs as buff}
+    <div>
+        <div class="rarity {buff.rarity}">
+
+        </div>
+        <h1>{buff.packName}</h1>
+        <h2 class="{buff.tier}">{buff.tier} Tier</h2>
+        <h3>{buff.listings} {buff.listings === 1 ? 'listing' : 'listings' }</h3>
+    </div>
+{/each}
--- shop/src/routes/marketplace.svelte
+++ shop/src/routes/marketplace.svelte
@@ -1,1 +1,11 @@
+<script>
+    import Buff from "../components/Buff.svelte";
+</script>
+
 <h1>Marketplace works!</h1>
+
+<div style="display: flex; flex-wrap: wrap">
+    <div style="width: 20%">
+        <Buff></Buff>
+    </div>
+</div>
