02ea3e37084c5861757a29d9d34d44bbdc173881
kristiantarnaj <kristiantarnaj@gmail.com>
2021-03-11 13:09:52 +0100

    feat(): added routing

--- shop/src/components/Nav.svelte
+++ shop/src/components/Nav.svelte
@@ -50,11 +50,11 @@
 
 <nav>
 	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">BLUFFS</a></li>
+		<li><a aria-current="{segment === 'packs' ? 'page' : undefined}" href="packs">PACKS</a></li>
+		<li><a aria-current="{segment === 'marketplace' ? 'page' : undefined}" href="marketplace">MARKETPLACE</a></li>
+		<li><a aria-current="{segment === 'leaderboard' ? 'page' : undefined}" href="leaderboard">LEADERBOARD</a></li>
+		<li><a aria-current="{segment === 'login' ? 'page' : undefined}" href="login">LOG IN</a></li>
+		<li><a aria-current="{segment === 'register' ? 'page' : undefined}" href="register">REGISTER</a></li>
 	</ul>
 </nav>
---
+++ shop/src/routes/marketplace.svelte
@@ -0,0 +1,1 @@
+<h1>Marketplace works!</h1>
