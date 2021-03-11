d40473e4cc1528259572951edc1bc0ea13ac9210
kristiantarnaj <kristiantarnaj@gmail.com>
2021-03-11 13:40:56 +0100

    feat(): added packs

--- shop/src/routes/_layout.svelte
+++ shop/src/routes/_layout.svelte
@@ -6,12 +6,10 @@
 
 <style>
 	main {
-		position: relative;
-		max-width: 56em;
+		max-width: 1200px;
 		background-color: white;
-		padding: 2em;
+		padding-top: 2em;
 		margin: 0 auto;
-		box-sizing: border-box;
 	}
 </style>
 
--- shop/src/routes/packs.svelte
+++ shop/src/routes/packs.svelte
@@ -1,1 +1,9 @@
+<script>
+    import Pack from "../components/Pack.svelte";
+</script>
+
 <h1>Packs works!</h1>
+
+<div style="width: 20%">
+    <Pack></Pack>
+</div>
